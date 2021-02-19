a29b1ff6fc91e3df99146571c950fa0fad564c52
James Lew <jamessebastianlew@gmail.com>
2021-02-19 14:47:43 +1100

    n^5 solution implemented with recounting excavation issues

--- cf/1012c.cpp
+++ cf/1012c.cpp
@@ -22,6 +22,9 @@ using namespace std;
 
 const int mxn = 5e3+1;
 int dp[mxn][mxn]; // dp[l][r]
+const int mxm = 5e2+2;
+int bdp[mxm][mxm][mxm]; // stands for bad dp
+
 // for k=1 it is easy
 // for k=2 the only reason you would choose different to k=1
 // + making the next cheapest hill would be if you were on a different
@@ -29,16 +32,28 @@ int dp[mxn][mxn]; // dp[l][r]
 
 int n;
 int aa[mxn];
+
+int C(int i) {
+    int cost = 0;
+    if(i-1 >= 0 && aa[i-1] >= aa[i]) cost += aa[i-1] - (aa[i]-1);
+    if(i+1 < n && aa[i+1] >= aa[i]) cost += aa[i+1] - (aa[i]-1);
+    return cost;
+}
+
+int cc[mxn];
+
 int main() {
     cio();
     cin >> n;
     for(int i=0; i<n; ++i) cin >> aa[i];
+    for(int i=0; i<n; ++i) cc[i] = C(i);
+    cout << endl;
     /*
     simple O(n^5) algorithm
     dp[mxn][mxn][mxn];
 
     // find the best of height k
-    for(int k=1; k<=(K+1)/2; ++k) {
+    for(int k=1; k<=(n+1)/2; ++k) {
         // consider exclusive bound (l, r)
         for(int l=n; l>=-1; --l) for(int r=l+2; r<=n; ++r) {
             // choose an hill to be a house
@@ -51,6 +66,17 @@ int main() {
     // min from z=0 to k-1 of (C(i) + dp[l][i-1][z] + dp[i+1][r][k-1-z])
     */
 
+
+    // implementing O(n^5)
+    memset(bdp, 0x3f, sizeof(bdp));
+    for(int k=0; k<=(n+1)>>1; ++k) for(int l=n+1; l>=0; --l)
+    for(int r=l; r<=n+1; ++r) {
+        if(k==0) bdp[l][r][k] = 0;
+        else for(int i=l+1; i<r; ++i) for(int z=0; z<=k-1; ++z)
+            amin(bdp[l][r][k], cc[i-1] + bdp[l][i-1][z] + bdp[i+1][r][k-1-z]);
+    }
+    for(int k=1; k<=(n+1)>>1; ++k) cout << bdp[0][n+1][k] << ' ';
+    cout << endl;
 
     return 0;
 }
